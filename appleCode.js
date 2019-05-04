c1db9021479605a47cceb35f873fab1e88025a44
Jon <deathcaller120@outlook.com>
2019-05-03 21:33:57 -0700

    Update appleCode.js

--- appleCode.js
+++ appleCode.js
@@ -470,7 +470,7 @@ function appleGain(){
 document.addEventListener('keydown', function(event) {
 	if (event.keyCode == 32) {
 		setTimeout(appleGain, 10);
-	} else if (event.keyCode == 13 && app >= 0){
+	} else if (event.keyCode == 13 && app >= 1){
 		app--;
 	} else {
 		return;
